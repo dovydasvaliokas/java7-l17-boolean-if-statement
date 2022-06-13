41d028e8888ffb9771e2497e38ac7d0d361ee361
dovydasvaliokas <74957396+dovydasvaliokas@users.noreply.github.com>
2022-06-13 13:21:40 +0300

    Teigiamo neigiamo funkcija sukurta, pradeta lyginimo funkcija

--- antras-pvz-su-ifais/script.js
+++ antras-pvz-su-ifais/script.js
@@ -1,3 +1,32 @@
 window.onload = function() {
-    
+    let x = 25;
+    let y = -100;
+
+    console.log(arSkaiciusYraTeigiamas(x));
+    console.log(arSkaiciusYraTeigiamas(y));
 }
+
+
+/**
+ * Funkcija patikrina ar skaičius yra neigiamas ar teigiamas.
+ * @param {*} skaicius - skaičius, kurį tikrina
+ * @returns grąžina true, jeigu skaičius yra teigiamas; grąžina false - jeigu neigiamas.
+ */
+function arSkaiciusYraTeigiamas(skaicius) {
+    if (skaicius > 0) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+
+
+/**
+ * Ji padaro tą ir tą
+ */
+function palygintiDuSkaicius() {
+
+}
+
+
